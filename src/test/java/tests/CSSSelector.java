797ca9136d579d48e010a2ab88cf0db889857ff0
Lenovo <sea20@mail.com>
2021-12-08 21:49:59 +0300

    Lesson 7

---
+++ src/test/java/tests/CSSSelector.java
@@ -0,0 +1,62 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CSSSelector {
+
+
+    private void cssSelector () {
+        By classSelector = By.cssSelector(".radio"); // пойск по классу radio
+        By classSelector1 = By.className(".radio"); // поиск по названию класса одинаковые с первым
+
+        By idSelector = By.cssSelector("#suite_mode_single"); // пойск по ID
+        By idSelector1 = By.id("suite_mode_single"); // поиск по ID
+
+        By tagSelector = By.cssSelector("label"); // пойск по tag
+        By tagSelector1 = By.tagName("label"); // поиск по tag
+
+        By tagAttributSelector = By.cssSelector("label[for='name']"); // поиск по tag и атрибуту со значением
+        By tagAttributSelector = By.cssSelector("label[for]"); // поиск по tag и атрибуту без значением
+
+        By multipleClassesSelector = By.cssSelector(".column.overflow-content"); // поиск по двум классам в элементе
+
+        By simpleHierarchicalSelector = By.cssSelector(".table .content-inner"); // поиск простого дочернего элемента
+
+        By fullHierarchicalSelector = By.cssSelector("body .table .content-inner"); // поиск c начала простого дочернего элемента
+
+        By searchLikeContainsSelector = By.cssSelector("for[action*='admin']"); // поиск c вхождением подстроки в значении атрибута
+        By searchBYWordSelector = By.cssSelector("for[action~='admin']"); // поиск c вхождением слова в значении атрибута
+
+        By valueStartsFromSelector = By.cssSelector ("a[href^='https://']");  // поиск элемента с аттрибутом значение которого начинается с...
+
+        By valueEndsWithSelector = By.cssSelector ("a[href$='@gmail.com']");  // поиск элемента с аттрибутом значение которого заканчивается на...
+
+        By childAfterParentsSelector = By.cssSelector("ul>li"); // поиск дочернего li у которого родитель ul
+
+        By elementRightAfterElementSelector = By.cssSelector("#form+script"); // поиск элемента script который идет сразу после элемента #form
+        By elementOnTheSameLevelSelector = By.cssSelector("#form~script"); // поиск элементов script которым предшевствует элемент #form
+
+        By firstChildSelector = By.cssSelector("li:first-child"); //поиск первого дочерненго элемента
+        By lastChildSelector = By.cssSelector("li:first-child"); //поиск последнего дочерненго элемента
+
+        By nthChildFromBeginSelector = By.cssSelector("li:nth-child(2)"); // поиск эннего дочернего элемента c начала
+        By nthlastChildSelector = By.cssSelector("li:nth-last-child(3)"); // поиск эннего дочернего элемента с конца
+
+
+
+        li:first-child
+        li:last-child
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
