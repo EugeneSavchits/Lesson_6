1f50a06ed761708e11af231e7032fa633dacdb0a
Lenovo <sea20@mail.com>
2021-12-12 05:45:37 +0300

    HomeWork_2_Css

--- src/test/java/tests/CSSSelector.java
+++ src/test/java/tests/CSSSelector.java
@@ -8,7 +8,7 @@ public class CSSSelector {
 
     private void cssSelector () {
         By classSelector = By.cssSelector(".radio"); // пойск по классу radio
-        By classSelector1 = By.className(".radio"); // поиск по названию класса одинаковые с первым
+        By classSelector1 = By.className("radio"); // поиск по названию класса одинаковые с первым
 
         By idSelector = By.cssSelector("#suite_mode_single"); // пойск по ID
         By idSelector1 = By.id("suite_mode_single"); // поиск по ID
@@ -17,31 +17,31 @@ public class CSSSelector {
         By tagSelector1 = By.tagName("label"); // поиск по tag
 
         By tagAttributSelector = By.cssSelector("label[for='name']"); // поиск по tag и атрибуту со значением
-        By tagAttributSelector = By.cssSelector("label[for]"); // поиск по tag и атрибуту без значением
+        By tagAttributSelector1 = By.cssSelector("label[for]"); // поиск по tag и атрибуту без значением
 
         By multipleClassesSelector = By.cssSelector(".column.overflow-content"); // поиск по двум классам в элементе
 
         By simpleHierarchicalSelector = By.cssSelector(".table .content-inner"); // поиск простого дочернего элемента
 
-        By fullHierarchicalSelector = By.cssSelector("body .table .content-inner"); // поиск c начала простого дочернего элемента
+        By fullHierarchicalSelector = By.cssSelector("body .table .content-inner"); // использование трехуровневой иерархии для поиска дочернего элемента
 
-        By searchLikeContainsSelector = By.cssSelector("for[action*='admin']"); // поиск c вхождением подстроки в значении атрибута
-        By searchBYWordSelector = By.cssSelector("for[action~='admin']"); // поиск c вхождением слова в значении атрибута
+        By searchLikeContainsSelector = By.cssSelector("form[action*='admin']"); // поиск c вхождением подстроки в значении атрибута
+        By searchBYWordSelector = By.cssSelector("form[action~='admin']"); // поиск c вхождением слова в значении атрибута
 
         By valueStartsFromSelector = By.cssSelector ("a[href^='https://']");  // поиск элемента с аттрибутом значение которого начинается с...
 
         By valueEndsWithSelector = By.cssSelector ("a[href$='@gmail.com']");  // поиск элемента с аттрибутом значение которого заканчивается на...
 
-        By childAfterParentsSelector = By.cssSelector("ul>li"); // поиск дочернего li у которого родитель ul
+        By childRightAfterParentsSelector = By.cssSelector("ul>li"); // поиск дочернего li у которого родитель ul
 
-        By elementRightAfterElementSelector = By.cssSelector("#form+script"); // поиск элемента script который идет сразу после элемента #form
-        By elementOnTheSameLevelSelector = By.cssSelector("#form~script"); // поиск элементов script которым предшевствует элемент #form
+        By elementRightAfterElementSelector = By.cssSelector("#form+script"); // поиск элемента script который идет сразу после элемента #form на том же уровне
+        By elementOnTheSameLevelSelector = By.cssSelector("#form~script"); // поиск элементов script которым предшевствует элемент #form на том же уровне
 
-        By firstChildSelector = By.cssSelector("li:first-child"); //поиск первого дочерненго элемента
-        By lastChildSelector = By.cssSelector("li:first-child"); //поиск последнего дочерненго элемента
+        By firstChildSelector = By.cssSelector("li:first-child"); //поиск первого дочерненго элемента с тэгом li на одном уровне
+        By lastChildSelector = By.cssSelector("li:last-child"); //поиск последнего дочерненго элемента с тэгом li на одном уровне
 
-        By nthChildFromBeginSelector = By.cssSelector("li:nth-child(2)"); // поиск эннего дочернего элемента c начала
-        By nthlastChildSelector = By.cssSelector("li:nth-last-child(3)"); // поиск эннего дочернего элемента с конца
+        By nthChildFromBeginSelector = By.cssSelector("li:nth-child(2)"); // поиск эннего дочернего элемента c начала с тэгом li на одном уровне
+        By nthlastChildSelector = By.cssSelector("li:nth-last-child(3)"); // поиск эннего дочернего элемента с конца с тэгом li на одном уровне
 
 
 
--- src/test/java/tests/HomeWork_2_Css.java
+++ src/test/java/tests/HomeWork_2_Css.java
@@ -1,4 +1,64 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class HomeWork_2_Css {
+    static String URL = "https://babariko.vision/";
+
+    @Test
+    public void Css () throws InterruptedException {
+
+        //НЕ МОГУ ТОЛЬКО ПОНЯТЬ ПОЧЕМУ ПРИ ЗАПУСКЕ ТЕСТА НЕ ОТКРЫВАЕТСЯ САЙТ, ХОТЯ В БРАУЗЕРЕ ВСЕ ОТКРЫВАЛОСЬ, ТЕСТ ЗАПУСТИЛ ПЕРВЫЙ РАЗ КОГДА УЖЕ ВСЕ СДЕЛАЛ!
+
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(URL);
+        Thread.sleep(5000);
+
+
+        WebElement classSelector =driver.findElement(By.cssSelector(".l-section-slider")); // пойск по классу
+
+        WebElement idSelector = driver.findElement(By.cssSelector("#page-header")); // пойск по ID
+
+        WebElement tagSelector = driver.findElement(By.cssSelector("body")); // пойск по tag
+
+
+        WebElement tagAttributeValueSelector =driver.findElement(By.cssSelector("header[id='page-header']")); // поиск по tag и атрибуту со значением
+
+        WebElement tagAttributeSelector = driver.findElement(By.cssSelector("header[id]")); // поиск по tag и присутствием атрибута
+
+        WebElement multipleClassesSelector =driver.findElement(By.cssSelector(".w-image.hide-for-not-sticky")); // поиск по двум классам в элементе
+
+        WebElement simpleHierarchicalSelector = driver.findElement(By.cssSelector(".w-image.hide-for-not-sticky .attachment-full.size-full")); // поиск простого дочернего элемента
+
+        WebElement fullHierarchicalSelector = driver.findElement(By.cssSelector("div .wpb_wrapper .attachment-full.size-full")); // использование трехуровневой иерархии для поиска дочернего элемента
+
+        WebElement searchLikeContainsSelector = driver.findElement(By.cssSelector("img[src*='kupreeva']")); // поиск c вхождением подстроки в значении атрибута
+
+        WebElement searchBYWordSelector = driver.findElement(By.cssSelector("div[style~='url(\"https://i.ytimg.com/vi/v7q_SKxNtW8/maxresdefault.jpg\");']")); // поиск c вхождением слова в значении атрибута
+
+        WebElement valueStartsFromSelector = driver.findElement(By.cssSelector("a[class^='js']"));  // поиск элемента с аттрибутом значение которого начинается с...
+
+        WebElement valueEndsWithSelector = driver.findElement(By.cssSelector("a[class$='toggle']"));  // поиск элемента с аттрибутом значение которого заканчивается на...
+
+        WebElement childRightAfterParentsSelector = driver.findElement(By.cssSelector("svg>g")); // поиск дочернего g у которого родитель svg
+
+        WebElement elementRightAfterElementSelector = driver.findElement(By.cssSelector("#menu-item-15474+li")); // поиск элемента c тэгом li который идет сразу после элемента #menu-item-15474 на том же уровне
+
+        WebElement elementOnTheSameLevelSelector = driver.findElement(By.cssSelector("#menu-item-15474~#menu-item-8900")); // поиск элемента #menu-item-8900 которому предшевствует элемент #menu-item-15474 на томже уровне
+
+        WebElement firstChildSelector =driver.findElement(By.cssSelector("ul:first-child")); //поиск первого дочерненго элемента с тэгом ul на одном уровне
+
+        WebElement lastChildSelector = driver.findElement(By.cssSelector("ul:first-child")); //поиск последнего дочерненго элемента с тэгом ul на одном уровне
+
+        WebElement nthChildFromBeginSelector =driver.findElement(By.cssSelector("li:nth-child(4)")); // поиск n-го дочернего элемента c начала на одном уровне
+
+        WebElement nthChildFromEndSelector =driver.findElement(By.cssSelector("li:nth-last-child(6)")); // поиск n-го дочернего элемента с конца на одном уровне
+
+        driver.quit();
+    }
 }
