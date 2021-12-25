13b7e19ee2f5a97bb4a7c3cda1cd269cafacbc9b
Lenovo <sea20@mail.com>
2021-12-26 01:17:27 +0300

    Waits

---
+++ src/main/java/baseEntities/BaseTest.java
@@ -0,0 +1,53 @@
+package baseEntities;
+
+import core.ReadProperties;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+
+    @BeforeMethod
+    public void setUp (){
+        switch (ReadProperties.getBrowserType().toLowerCase()) {
+            case "chrome":
+                WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--silent");
+                chromeOptions.setHeadless(ReadProperties.getHeadless());
+
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
+            case "edge":
+                WebDriverManager.getInstance(DriverManagerType.EDGE).setup();
+
+                driver = new EdgeDriver();
+                break;
+            default:
+                System.out.println("This type of browser is not supported");
+                break;
+
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        driver.get(ReadProperties.getUrl());
+    }
+
+    @AfterMethod
+    public void tearDown () {
+        driver.quit();
+
+    }
+}
