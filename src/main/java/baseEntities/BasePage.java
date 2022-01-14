efb2696c8592413d44ce5b56485744780ab8f729
Lenovo <sea20@mail.com>
2022-01-14 03:10:52 +0300

    LoadableComponent

--- src/main/java/baseEntities/BasePage.java
+++ src/main/java/baseEntities/BasePage.java
@@ -3,10 +3,11 @@ package baseEntities;
 
 import Utils.Waits;
 import core.ReadProperties;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public abstract class BasePage {
+    protected static final int WAIT_FOR_PAGE_LOADED_IN_SECONDS = 30;
+
     protected WebDriver driver;
     protected Waits waits;
     protected String BASE_URL;
@@ -23,11 +24,24 @@ public abstract class BasePage {
         if (openPageByUrl) {
             openPage();
         }
+        waitForOpen();
     }
 
     protected abstract void openPage();
+    protected abstract boolean isPageOpened();
 
-    public boolean isPageOpened(By by) {
-       return waits.waitForVisibility(by).isDisplayed();
-       }
+
+    protected void waitForOpen() {
+        int tryCount = 0;
+        boolean isPageOpenedIndicator = isPageOpened();
+
+        while (!isPageOpenedIndicator && tryCount < (WAIT_FOR_PAGE_LOADED_IN_SECONDS/ReadProperties.getTimeOut())) {
+            tryCount++;
+            isPageOpenedIndicator = isPageOpened();
+        }
+
+        if (!isPageOpenedIndicator) {
+            throw new AssertionError("Page was not opened");
+        }
+    }
 }
