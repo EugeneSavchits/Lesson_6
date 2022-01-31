48563b35b3b1793a39a7b597a6f597ea0e0cab45
Lenovo <sea20@mail.com>
2022-01-31 16:33:58 +0300

    wrappers

--- src/test/java/Utils/Endpoints.java
+++
@@ -1,8 +0,0 @@
-package Utils;
-
-public class Endpoints {
-
-    public static final String GET_ALL_USERS = "/index.php?/api/v2/get_users";
-    public static final String GET_USER = "/index.php?/api/v2/get_user/{id}";
-
-}
