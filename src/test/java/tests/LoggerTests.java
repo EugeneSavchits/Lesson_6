5599a097d7b61a0f0597e46057ced884e29da24e
Lenovo <sea20@mail.com>
2022-02-03 23:58:30 +0300

    logger

--- src/test/java/tests/LoggerTests.java
+++ src/test/java/tests/LoggerTests.java
@@ -1,7 +1,34 @@
 package tests;
 
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
 
 public class LoggerTests {
-    public static final Logger logger
+    private static final Logger logger = LogManager.getLogger(LoggerTests.class);
+
+    @Test
+    public void logTest(){
+        logger.trace("Trace Message: Начало теста");
+        logger.debug("Debug Message: ...");
+        logger.info("Info Message: ...");
+        logger.error("Error Message: ...");
+        logger.fatal("Fatal Message: ....");
+    }
+
+    @Test
+    public void loginTest(){
+        logger.trace("login test is started...");
+
+        logger.trace("Инициализируем начальные переменные...");
+        int a = 10;
+        int b = 0;
+
+        if(b == 0){
+            logger.fatal("Делитель равен нулю...");
+        }
+        int c = a/b;
+
+        logger.trace("login test is finished...");
+    }
 }
