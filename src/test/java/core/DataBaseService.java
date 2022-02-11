10892799520112013379999015133f5b825efb3e
Lenovo <sea20@mail.com>
2022-02-11 17:07:13 +0300

    db

--- src/test/java/core/DataBaseService.java
+++ src/test/java/core/DataBaseService.java
@@ -1,9 +1,8 @@
 package core;
 
+import org.apache.log4j.Logger;
 
-import org.testng.log4testng.Logger;
-
-import java.sql.Connection;
+import java.sql.*;
 
 public class DataBaseService {
     public static Logger logger = Logger.getLogger(DataBaseService.class);
@@ -13,11 +12,74 @@ public class DataBaseService {
     static final String PASSWORD = "postgres";
 
     Connection connection = null;
+    Statement statement = null;
 
-    public DataBaseService(){
-        try{
+    public DataBaseService() {
+        try {
             Class.forName("org.postgresql.Driver");
-        }catch ()
+        } catch (ClassNotFoundException e) {
+            logger.fatal(e.toString());
+            return;
+        }
 
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            logger.error(e.toString());
+            return;
+        }
+
+        if (connection != null) {
+            logger.info("Мы подключились к БД...");
+        }
+    }
+
+    public Statement getStatement() {
+        try {
+            if (statement == null) {
+                statement = connection.createStatement();
+            }
+        } catch (SQLException e) {
+            logger.info(e.toString());
+        }
+
+        return statement;
+    }
+
+    public void executeSQL(String sql) {
+        try {
+            logger.info("Результат выполнения запроса: " + getStatement().execute(sql));
+        } catch (SQLException e) {
+            logger.info(e.toString());
+        }
+    }
+
+    public ResultSet executeQuery(String sql) {
+        try {
+            return getStatement().executeQuery(sql);
+        } catch (SQLException e) {
+            logger.info(e.toString());
+        }
+
+        return null;
+    }
+
+    public void closeStatement() {
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+        } catch (SQLException e) {
+            logger.info(e.toString());
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            logger.info(e.toString());
+            return;
+        }
     }
 }
